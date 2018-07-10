1580a5c9e748d09812a9f6cbe22e6c2959047264
franz.silva@fundacion-jala.org <franz.silva@fundacion-jala.org>
2018-07-10 15:15:05 -0400

    searchByPath

---
+++ src/com/fundation/search/model/Search.java
@@ -0,0 +1,38 @@
+package com.fundation.search.model;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * this class is about search path with diferent criteria.
+ */
+public class Search {
+
+    private List<File> fileList;
+
+    /**
+     * Search Class constructor.
+     */
+    public Search() {
+        fileList = new ArrayList<>();
+    }
+
+    /**
+     * search by path.
+     * @param path is a String that contains path
+     * @return list of Files
+     */
+    public  List<File> searchByPath(String path) {
+        try {
+            File[] files = new File(path).listFiles();
+            for (File file : files) {
+                fileList.add(file);
+                if (file.isDirectory()) {
+                    searchByPath(file.getPath());
+                }
+            }
+        } catch (NullPointerException e) {
+        }
+        return fileList;
+    }
+}
