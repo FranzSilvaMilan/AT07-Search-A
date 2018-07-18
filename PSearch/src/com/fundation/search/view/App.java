a44b0f50058b4d4051c46016f0b992401d997c56
ketty <72768163ketty@gmail.com>
2018-07-17 23:30:14 -0400

    ketty VIEW interface

---
+++ PSearch/src/com/fundation/search/view/App.java
@@ -0,0 +1,13 @@
+package com.fundation.search.view;
+
+/**
+ * This class main of the interface.
+ *q
+ * @author Ketty Camacho Vasquez.
+ */
+public class App {
+    public static void main(String[] arg) {
+        new FrameMain();
+    }
+
+}
