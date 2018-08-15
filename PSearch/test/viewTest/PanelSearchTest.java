6364ac48278d5045695ff408bf086f5b4234576f
ketty <72768163ketty@gmail.com>
2018-08-15 15:03:26 -0400

    test

--- PSearch/test/viewTest/PanelSearchTest.java
+++ PSearch/test/viewTest/PanelSearchTest.java
@@ -14,21 +14,64 @@ public class PanelSearchTest {
         panelS = new PanelSearch();
     }
 
+    /**
+     *
+     */
     @Test
     public void testSetOnlyReadFalse(){
         panelS.setEnableOnlyRead(false);
         assertFalse(panelS.getOnlyRead());
     }
+    /**
+     *
+     */
     @Test
     public void testSetKeysensitiveTrue(){
         panelS.setEnableKeySensitive(true);
         assertTrue(panelS.getKeySensitive());
     }
+    /**
+     *
+     */
     @Test
     public void testSetKeysensitiveFalse(){
         panelS.setEnableKeySensitive(false);
         assertFalse(panelS.getKeySensitive());
     }
-
+    /**
+     *
+     */
+    @Test
+    public void testSetStartWithTrue(){
+        panelS.setStartWith(true);
+        assertTrue(panelS.getStartWith());
+    }
+    /**
+     *
+     */
+    @Test
+    public void testSetStartWithFalse(){
+        panelS.setStartWith(false);
+        assertFalse(panelS.getStartWith());
+    }
+    /**
+     *
+     */
+    @Test
+    public void testSetEndWhithTrue(){
+        panelS.setEndWith(true);
+        assertTrue(panelS.getEndWith());
+    }
+    /**
+     *
+     */
+    @Test
+    public void testSetFolderTrue(){
+        panelS.setFolder(true);
+        assertTrue(panelS.getSearchFolder());
+    }
+    /**
+     *
+     */
 
 }
